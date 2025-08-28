c173f613551609ad4c61d6a306393e425a06c42a
seita-uchida <seita_uchida@waotech.jp>
2025-08-28 21:20:00 +0900

    4-モデル・管理者サイト

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),  # name = "index" を追加
-    path("results/", views.results, name="results"),  # この行を追加
 ]
--- main/views.py
+++ main/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    question_list = [
-        "プログラミングは好きですか？",
-        "数学は好きですか？",
-        "国語は好きですか？",
-    ]
+    all_question = Question.objects.all()
     context = {
-        "question_list": question_list,  # list 型を渡す
-        "is_polled": True,
-        "polled_msg": "投票ありがとうございました。",
-        "not_polled_msg": "投票をお願いします。",
+        "all_question": all_question,
     }
     return render(request, "main/index.html", context)
-
-
-def results(request):
-    question_dict = {
-        "プログラミングは好きですか？": True,
-        "数学は好きですか？": True,
-        "国語は好きですか？": True,
-    }
-    context = {
-        "question_dict": question_dict,  # dict 型を渡す
-    }
-    return render(request, "main/results.html", context)
